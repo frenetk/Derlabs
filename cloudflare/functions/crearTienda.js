eca10b8660099e05229c6512d1732ce7322eec05
frenetk <espinozar.diego@gmail.com>
2026-09-18 09:15:59 -0300

    Fix slugify: guion bajo en storeId

--- cloudflare/functions/crearTienda.js
+++ cloudflare/functions/crearTienda.js
@@ -26,7 +26,7 @@ function slugify(texto){
   return String(texto || "")
     .toLowerCase()
     .normalize("NFD").replace(/[\u0300-\u036f]/g, "")
-    .replace(/[^a-z0-9]+/g, "-")
+    .replace(/[^a-z0-9]+/g, "_")
     .replace(/^-+|-+$/g, "")
     .slice(0, 40) || "tienda";
 }
